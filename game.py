49b38a675d7c6ebd98351fe00e613adf611da5d7
Maciej Nowak <mcnowak@wp.pl>
2017-06-20 22:45:50 +0200

    Implemented classes Game and Ship

---
+++ game.py
@@ -0,0 +1,41 @@
+class Game:
+    def __init__(self):
+        """
+        Method creates an instance of the class
+
+        Returns:
+            None
+        """
+
+        self.players = {first: Player(), second: Player()}
+
+    def check_whose_turn(self):
+        """
+        Method checks both objects of class Player and returns Player with attribute my_turn = True
+
+        Returns:
+            obj of class Player
+        """
+
+        for key in self.players:
+            if self.players[key].my_turn:
+                return self.players[key]
+
+    def switch_turn(self):
+        """
+        Method switches both players my_turn attributes to opposed
+
+        Returns:
+            None
+        """
+
+        for key in self.players:
+            self.players[key].my_turn = not my_turn
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ ship.py
@@ -0,0 +1,77 @@
+class Ship:
+    def __init__(self, start_row, start_column, name, is_vertical):
+        """
+        Method creates an instance of the class
+
+        Returns:
+            None
+        """
+
+        self.name = name
+        self.start_row = start_row
+        self.start_column = start_column
+        self.is_vertical = is_vertical
+        self.length = self.get_ship_length()
+        self.squares = []
+        self.generate_ship()
+
+    def get_ship_length(self):
+        """
+        Method returns ship length which depends on the name
+
+        Returns:
+            length (int): ship length
+        """
+
+        ship_lengths = {'Carrier': 5, 'Battleship': 4, 'Cruiser': 3, 'Submarine': 3, 'Destroyer': 2}
+        length = ship_lengths[self.name]
+        return length
+
+    def generate_ship(self):
+        """
+        Method creates objects of Square class and appends them to the list
+
+        Returns:
+            None
+        """
+
+        for i in range(self.length):
+            if self.is_vertical:
+                self.squares.append(Square(self.start_row + i, self.start_column))
+            else:
+                self.squares.append(Square(self.start_row, self.start_column + i))
+            self.squares[-1].change_state('O')
+
+    @property
+    def is_hit(self):
+        for square in self.squares:
+            if square.state = 'X':
+                return True
+        return False
+
+    @property
+    def is_sunk(self):
+        for square in self.squares:
+            if square.state = 'O':
+                return False
+        return True
+
+    def __str__(self):
+        """
+        Method overloads the str operator
+
+        Returns:
+            str_ship (str): string used by print function
+        """
+
+        str_ship = [square.state for square in self.squares]
+        str_ship = ''.join(str_ship)
+        return str_ship
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
