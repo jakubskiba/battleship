9a63fe15aca8c4329313245e37696fc8fd775475
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-22 00:46:50 +0200

    Change name of variable with generated by AI coordinates (x, y) of square to hit

--- AI/artificial_intelligence.py
+++ AI/artificial_intelligence.py
@@ -6,20 +6,23 @@ from AI.level.normalAI import NormalAI
 class ArtificialIntelligence:
     def __init__(self):
         self.level = DifficultyLevel()
-        self.coordX = self.generate_hit()[0]
-        self.coordY = self.generate_hit()[1]
+        self.coordX = self.determine_where_to_hit()[0]
+        self.coordY = self.determine_where_to_hit()[1]
 
-    def generate_hit(self):
-        drawn_coordinates = None
+    def determine_where_to_hit(self):
+        """
+        Method to choose where to hit -> which depends on difficulty level.
+        """
+        hit_coordinates = None
 
         if self.level == 1:
-            drawn_coordinates = EasyAI().draw_location_to_hit()
+            hit_coordinates = EasyAI().draw_location_to_hit()
         elif self.level == 2:
-            drawn_coordinates = NormalAI().draw_location_to_hit()
+            hit_coordinates = NormalAI().draw_location_to_hit()
         elif self.level == 3:
             pass
             # hardAI.xxx()
         else:
             raise ValueError("Invalid difficulty level")
 
-        return drawn_coordinates
+        return hit_coordinates
