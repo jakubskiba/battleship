24325a8bc5748925ed1bbfd04da3593b63def809
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-22 00:10:24 +0200

    Add docstrings, change name of methods to generate hit locations in abstractAI and easyAI modules

--- AI/level/easyAI.py
+++ AI/level/easyAI.py
@@ -6,8 +6,11 @@ class EasyAI(AbstractAI):
     def __init__(self):
         super().__init__()
 
-    def hit_location(self):
-        row_num = random.randint(0, len(self.ocean.board[0])-1)
-        column_num = random.randint(0, len(self.ocean.board[0])-1)
+    def draw_location_to_hit(self):
+        """
+        Generate which coordinates should be typed as a candidate to hit by algorithm.
+        """
+        row_num = random.randint(0, len(self.ocean.board[0]) - 1)
+        column_num = random.randint(0, len(self.ocean.board[0]) - 1)
         if self.ocean.board[row_num][column_num].can_be_hit():
             return row_num, column_num
