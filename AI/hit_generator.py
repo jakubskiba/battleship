c72842214eb684504fa0c145138794cda5d1d386
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-21 13:34:08 +0200

    Refactor import statement to operate on object more clearly

--- AI/hit_generator.py
+++ AI/hit_generator.py
@@ -1,16 +1,17 @@
-import difficulty_level
+from difficulty_level import DifficultyLevel
+import easyAI
 
 
 class HitGenerator:
     def __init__(self):
-        self.level = difficulty_level.DifficultyLevel()
+        self.level = DifficultyLevel()
 
     def generate_hit(self):
         if self.level == 1:
-            easyAI.xxx()
+            easyAI
         elif self.level == 2:
-            normalAI.xxx()
+            # normalAI.xxx()
         elif self.level == 3:
-            hardAI.xxx()
+            # hardAI.xxx()
         else:
             raise ValueError("Invalid level")
