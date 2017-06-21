446d3c6002ed58875d34b8af0fe575a7b7da12c1
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-22 01:13:53 +0200

    Delete import of DifficultyLevel, move magic numbers in constructor, change index of coordinates, add parameter 'level' to constructor

--- AI/artificial_intelligence.py
+++ AI/artificial_intelligence.py
@@ -1,17 +1,23 @@
-from AI.difficulty_level import DifficultyLevel
 from AI.level.easyAI import EasyAI
 from AI.level.normalAI import NormalAI
 
 
 class ArtificialIntelligence:
-    def __init__(self):
-        self.level = DifficultyLevel()
-        self.coordX = self.determine_where_to_hit()[0]
-        self.coordY = self.determine_where_to_hit()[1]
+
+    def __init__(self, level):
+        position_y = 0
+        position_x = 1
+
+        self.level = level
+        self.coordY = self.determine_where_to_hit()[position_y]
+        self.coordX = self.determine_where_to_hit()[position_x]
 
     def determine_where_to_hit(self):
         """
         Method to choose where to hit -> which depends on difficulty level.
+
+        :returns
+            tuple of coordinates (y, x)
         """
         hit_coordinates = None
 
