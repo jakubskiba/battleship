34929dd2d4fc9b2dc13860ff3cd393b550e97ff3
Dubson69 <dubielgr@gmail.com>
2017-06-22 12:17:00 +0200

    Replace ocean.board to enemy_board in all methods of HardAI

--- AI/level/hardAI.py
+++ AI/level/hardAI.py
@@ -14,18 +14,19 @@ class HardAI(AbstractAI):
     def calculate_probability(self):
         hitable_squares = 0
         ship_squares = 0
-        for row in self.ocean.board: # sprawdzić po ktorej planszy iterujemy
+        for row in self.enemy_board:
             for square in row:
                 if square.state == "~":
                     hitable_squares += 1
-                elif square.state == "□":
+                elif square.state == "□": # sprawdzić czy kwadrat jest statkiem
                     ship_squares += 1
 
         return ship_squares/hitable_squares
 
     def set_square_probability(self):
-        for row in self.ocean.board:
+        for row in self.enemy_board:
             for square in row:
                 square.probability = self.calculate_probability()
 
     def is_hit(self):
+        pass
