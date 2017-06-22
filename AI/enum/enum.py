d287e64b0ebb05df48c2b658025a84fde7953189
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-22 10:56:51 +0200

    Add fields of labels for difficulty levels

--- AI/enum/enum.py
+++ AI/enum/enum.py
@@ -1,5 +1,11 @@
+EASY = 1
+NORMAL = 2
+HARD = 3
+
+
 class Enum:
 
-    EASY = 1
-    NORMAL = 2
-    HARD = 3
+    def __init__(self):
+        self.easy = EASY
+        self.normal = NORMAL
+        self.hard = HARD
